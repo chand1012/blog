1532bd62ffe60b24f8ce1541958cd899cc5e4ed2
Chandler Lofland <chandler@chand1012.dev>
2021-07-10 17:22:11 -0400

    Add GitHub sponsors

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,15 @@ const Footer = () => {
             <div id="copyright">
                 © {new Date().getFullYear()} Chandler Lofland
             </div>
+            <div id="sponsors">
+                <span>My GitHub Sponsors:</span> <br/>
+                <a href="https://github.com/SvenWritesCode">
+                    <img style={{marginRight: 8}} src="https://avatars2.githubusercontent.com/u/7458951?s=460&u=f4826fe80efc759290675b6a4694ff4cf2a965fb&v=4" height="50" width="50"/>
+                </a>
+                <a href="https://github.com/RusseII">
+                    <img style={{marginRight: 8}} src="https://avatars.githubusercontent.com/u/15036618?v=4" height="50" width="50"/>
+                </a>
+            </div>
         </footer>
     )
 }
